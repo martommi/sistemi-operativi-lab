cd326272c22d1dd21e4528c33e69719fedfaa730
Tommi <martinitommaso@tutanota.com>
2025-07-14 00:15:31 +0200

    fix: fix response mem leak

--- src/net/response.c
+++ src/net/response.c
@@ -1,38 +1,49 @@
-#include "../../include/response.h"
-#include "../../include/message.h"
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "../../include/response.h"
+#include "../../include/message.h"
+#include "../../include/file-utils.h"
+
 response_t *create_response(ResponseCode code, message_t *message) {
     response_t *resp = malloc(sizeof(response_t));
     if (!resp) {
         perror("malloc()");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
     resp->code = code;
+
+    if (message == NULL) {
+        fprintf(stderr, "%s(): cannot assign a null message.", __func__);
+        free(resp);
+        return NULL;
+    }
+
     resp->payload = message;
     return resp;
 }
 
 void free_response(response_t *response) {
-    if (!response) return;
-    free_message(response->payload);
+    if (response == NULL) return;
+    free_message(&response->payload);
     free(response);
 }
 
 int print_response(FILE *f, response_t *resp) {
-    if (!f || !resp || !resp->payload || !resp->payload->content)
-        return 0;
+    if (!f || !resp || !resp->payload || !resp->payload->content) {
+        fprintf(stderr, "%s(): trying to print null response or with null args.", __func__);
+        return -1;
+    }
 
     for (size_t i = 0; i < resp->payload->size; i++) {
         const char *line = resp->payload->content[i];
         if (line) {
-            if (fwrite(line, sizeof(char), strlen(line), f) < strlen(line)) {
-                perror("fwrite()");
-                return 0;
+            if (write_all(fileno(f), line, strlen(line)) != strlen(line)) {
+                fprintf(stderr, "%s(): write failed.", __func__);
+                return -1;
             }
         }
     }
