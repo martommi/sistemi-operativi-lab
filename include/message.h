c7de385b65c75058eb156f0b63ff8c98df07a25d
Tommi <martinitommaso@tutanota.com>
2025-07-13 23:07:57 +0200

    fix: fix message mem leak

--- include/message.h
+++ include/message.h
@@ -10,11 +10,12 @@ typedef struct message {
 } message_t;
 
 /* message content string array allocation is caller responsibility. The free is library responsibility. */
-message_t *create_message(size_t size, char **content);
+message_t *create_message(uint32_t size, char **content);
 message_t *create_message_from_str(const char *str);
 /* Caller should allocate memory for message content but rely on lib function to free it. */
 void free_message(message_t *message);
-void write_message(int fd, message_t *message);
-void read_message(int fd, message_t **message);
+int write_message(int fd, message_t *message);
+/* Memory allocation responsibility is library's */
+int read_message(int fd, message_t **message);
 
 #endif /* MESSAGE_H */
